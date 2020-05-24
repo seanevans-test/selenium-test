9366f5aabd9c6253542b44ba533a723f6790d6b1
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-05-24 15:37:07 +0100

    set up the webdriver launcher

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,12 +1,21 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTests {
 
     private WebDriver driver;
 
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver")
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        System.out.println(driver.getTitle());
+    }
+
+    public static void main(String args[]){
+        BaseTests test = new BaseTests();
+        test.setUp();
     }
 }
