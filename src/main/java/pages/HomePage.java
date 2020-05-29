0809359f2a131d042b940333882b9cd716bf32af
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-05-29 11:52:52 +0100

    set up a testing framework in the main folder, first test is for clicking a webelement

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+    private By formAuthenticationLink = By.linkText("Form Authentication");
+
+    public HomePage(WebDriver driver){
+
+    }
+
+    public void clickFormAuthentication(){
+
+    }
+}
