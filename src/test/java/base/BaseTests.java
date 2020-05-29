c7a41f52d7c85bd6fbf51de8eb1ee9a12b59e5e4
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-05-29 12:31:38 +0100

    added a protected HomePage method and substantiated the method with a driver input

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -2,18 +2,24 @@ package base;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
+
+import java.util.List;
 
 public class BaseTests {
 
     private WebDriver driver;
+    protected HomePage homePage;
 
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        System.out.println(driver.getTitle());
+        homePage = new HomePage(driver);
+
         driver.quit();
 
     }
