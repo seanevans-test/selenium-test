257a514e6c2599f362c9c381b6e4eaa289350d1e
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-06-01 20:55:34 +0100

    added annotations to allow TestNG in the test using @BeforeClass, removed redundant method

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import pages.HomePage;
 
 import java.util.List;
@@ -13,19 +15,18 @@ public class BaseTests {
     private WebDriver driver;
     protected HomePage homePage;
 
+    @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
         homePage = new HomePage(driver);
+    }
 
+    @AfterClass
+    public void tearDown(){
         driver.quit();
-
     }
 
-    public static void main(String args[]){
-        BaseTests test = new BaseTests();
-        test.setUp();
-    }
 }
