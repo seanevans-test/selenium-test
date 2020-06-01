2a710fd76a53223cc0d6e12e243ddf420da674d5
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-06-01 20:12:33 +0100

    set up automated test for LoginPage for username/password and click into next page

--- src/test/java/login/LoginTests.java
+++ src/test/java/login/LoginTests.java
@@ -1,10 +1,16 @@
 package login;
 
 import base.BaseTests;
+import pages.LoginPage;
+import pages.SecureAreaPage;
 
 public class LoginTests extends BaseTests {
 
     public void testSuccessfulLogin(){
-
+        LoginPage loginPage = homePage.clickFormAuthentication();
+        loginPage.setUsername("tomsmith");
+        loginPage.setPassword("SuperSecretPassword!");
+        SecureAreaPage secureAreaPage = loginPage.ClickLoginButton();
+        secureAreaPage.getAlertText();
     }
 }
