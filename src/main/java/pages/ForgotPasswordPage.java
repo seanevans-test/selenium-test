5aaa03d8787b1c0085ce381e0bf9d8a1963812ae
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-06-02 21:39:45 +0100

    first commit for the ForgotPasswordPage and set variables

---
+++ src/main/java/pages/ForgotPasswordPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ForgotPasswordPage {
+
+    private WebDriver driver;
+    private By emailField = By.id("email");
+    private By retrievePasswordButton = By.id("form_submit");
+
+}
