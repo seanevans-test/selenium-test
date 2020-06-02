3079d57cfd1a71d6c34ef6160025809dc41d73ce
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-06-02 20:40:51 +0100

    first commmit on dropdown Test class, extended BaseTest and imported testNG

---
+++ src/test/java/dropdown/DropdownTests.java
@@ -0,0 +1,12 @@
+package dropdown;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+public class DropdownTests extends BaseTests {
+
+    @Test
+    public void testSelectOption(){
+        homePage.clickDropdown();
+    }
+}
