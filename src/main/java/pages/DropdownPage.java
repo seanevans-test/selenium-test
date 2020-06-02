7da682e982b28e34c807c8482c1b1b20dbd175a7
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-06-02 20:10:20 +0100

    implemented the selenium support with the select option and added to the framework the with the dropdown list webelement

--- src/main/java/pages/DropdownPage.java
+++ src/main/java/pages/DropdownPage.java
@@ -1,12 +1,28 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class DropdownPage {
 
     private WebDriver driver;
+    private By dropdown = By.id("dropdown");
 
     public DropdownPage(WebDriver driver){
         this.driver = driver;
+    }
+
+    public void selectFromDropdown(String option){
+        Select dropdownElement = new Select(driver.findElement(dropdown));
+        dropdownElement.selectByVisibleText(option);
+    }
+
+    public String getSelectedOption(){
+
+    }
+
+    private Select findDropdownElement(){
+        return new Select(driver.findElement(dropdown))
     }
 }
