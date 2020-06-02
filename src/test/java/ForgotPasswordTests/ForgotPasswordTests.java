4bbd0493cf874be1f7c9ed6d4251af620bff9b04
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-06-02 22:12:22 +0100

    fixed an issue with the test not feeding through from the Page

--- src/test/java/ForgotPasswordTests/ForgotPasswordTests.java
+++ src/test/java/ForgotPasswordTests/ForgotPasswordTests.java
@@ -1,4 +1,17 @@
 package ForgotPasswordTests;
 
-public class ForgotPasswordTests {
+import base.BaseTests;
+import org.testng.annotations.Test;
+import pages.ForgotPasswordPage;
+
+import static org.testng.Assert.assertEquals;
+
+public class ForgotPasswordTests extends BaseTests {
+
+    @Test
+    public void testRetrievePassword(){
+        var forgetPasswordPage = homePage.clickForgotPassword();
+        var emailSentPage = ForgotPasswordPage.retrievePassword("tau@example.com");
+        assertEquals(emailSentPage.getMessage(), "Your e-mail's been sent!", "Message is incorrect");
+    }
 }
