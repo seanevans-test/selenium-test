5337fa622db03b677c6b6a1f6f0b71e381c669e7
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-06-02 19:48:39 +0100

    first commit dropdown menu test framework page

---
+++ src/main/java/pages/DropdownPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class DropdownPage {
+
+    private WebDriver driver;
+
+    public DropdownPage(WebDriver driver){
+        this.driver = driver;
+    }
+}
