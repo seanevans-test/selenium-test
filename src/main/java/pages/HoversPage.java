4cbe1cdff56d721fb37caeb24a3dc34588a27aa5
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-06-04 20:16:15 +0100

    first use of Actions class with moveToElemts to test hovering over webElements

--- src/main/java/pages/HoversPage.java
+++ src/main/java/pages/HoversPage.java
@@ -1,11 +1,21 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class HoversPage {
     private WebDriver driver;
 
+    private By figureBox = By.className("figure");
+
     public HoversPage(WebDriver driver){
         this.driver = driver;
+    }
+
+    public void hoverOverFigure(int index){
+        Actions actions = new Actions(driver);
+        actions.moveToElement()
     }
 }
