684c213df1076441c28c9d3e3c55797a10267f8d
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-06-04 21:15:27 +0100

    fixed issue with Password test, finished working hovertest

--- src/main/java/pages/ForgotPasswordPage.java
+++ src/main/java/pages/ForgotPasswordPage.java
@@ -26,5 +26,4 @@ public class ForgotPasswordPage {
         enterEmail(email);
         return clickRetrievePassword();
     }
-
 }
--- src/test/java/ForgotPasswordTests/ForgotPasswordTests.java
+++ src/test/java/ForgotPasswordTests/ForgotPasswordTests.java
@@ -2,7 +2,6 @@ package ForgotPasswordTests;
 
 import base.BaseTests;
 import org.testng.annotations.Test;
-import pages.ForgotPasswordPage;
 
 import static org.testng.Assert.assertEquals;
 
@@ -11,7 +10,7 @@ public class ForgotPasswordTests extends BaseTests {
     @Test
     public void testRetrievePassword(){
         var forgetPasswordPage = homePage.clickForgotPassword();
-        var emailSentPage = ForgotPasswordPage.retrievePassword("tau@example.com");
+        var emailSentPage = forgetPasswordPage.retrievePassword("tau@example.com");
         assertEquals(emailSentPage.getMessage(), "Your e-mail's been sent!", "Message is incorrect");
     }
 }
