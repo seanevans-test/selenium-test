1d2a9d89cd0b5d8d10925d8e6ca3d5fd5f47f279
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-06-04 19:55:03 +0100

    first commit on HoversPage with driver and constructor added

---
+++ src/main/java/pages/HoversPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HoversPage {
+    private WebDriver driver;
+
+    public HoversPage(WebDriver driver){
+        this.driver = driver;
+    }
+}
