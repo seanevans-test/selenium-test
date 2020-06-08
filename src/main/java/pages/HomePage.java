4c4abc097d9bf8fc19dd13f070ac4c3fefb086d2
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-06-08 21:05:34 +0100

    added link from HomePage to HorizontalSliderPage

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -31,6 +31,11 @@ public class HomePage {
         return new KeyPressesPage(driver);
     }
 
+    public HorizontalSliderPage clickHorizontalSlider(){
+        clickLink("Horizontal Slider");
+        return new HorizontalSliderPage(driver);
+    }
+
     private void clickLink(String linkText){
         driver.findElement(By.linkText(linkText)).click();
 
