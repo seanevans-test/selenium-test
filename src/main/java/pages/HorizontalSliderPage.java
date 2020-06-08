0bb24ce301faafeaf3e33a7653e7e6d424f1562b
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-06-08 21:04:50 +0100

    first commit on HorizontalSliderPage

---
+++ src/main/java/pages/HorizontalSliderPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HorizontalSliderPage {
+
+    private WebDriver driver;
+
+    public HorizontalSliderPage(WebDriver driver){
+        this.driver = driver;
+    }
+}
