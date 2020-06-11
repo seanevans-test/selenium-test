e5d7a3b33b6ab263d345d2c7bee35bc7455b2967
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-06-11 18:53:50 +0100

    first commit on AlertsPage

---
+++ src/main/java/pages/AlertsPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class AlertsPage {
+
+    private WebDriver driver;
+
+    public AlertsPage(WebDriver driver){
+        this.driver = driver;
+    }
+}
