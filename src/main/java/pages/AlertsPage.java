35403ed053fa5bfd70c853540f33d1e007fb83cb
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-06-11 19:12:54 +0100

    connected the trigger alert method to the desired button on webPage through the xpath of the webelement

--- src/main/java/pages/AlertsPage.java
+++ src/main/java/pages/AlertsPage.java
@@ -1,12 +1,20 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class AlertsPage {
 
     private WebDriver driver;
+    private By triggerAlertButton = By.xpath(".//button[text()='Click for JS Alert']");
 
     public AlertsPage(WebDriver driver){
         this.driver = driver;
     }
+
+    public void triggerAlert(){
+        driver.findElement(triggerAlertButton).click();
+    }
+
+    public
 }
