a6d48fe5432fceb6f5e62eb27d28d0cfb84eb7ff
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-06-11 19:27:52 +0100

    test setup to test the first button on the JavaScriptAlerts page

--- src/test/java/alerts/AlertsTests.java
+++ src/test/java/alerts/AlertsTests.java
@@ -1,4 +1,17 @@
 package alerts;
 
-public class AlertsTests {
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class AlertsTests extends BaseTests {
+
+    @Test
+    public void testAcceptAlert(){
+        var alertsPage = homePage.clickJavaScriptAlerts();
+        alertsPage.triggerAlert();
+        alertsPage.acceptAlert();
+        assertEquals(alertsPage.getResult(), "You sucessfuly clicked an alert", "Results text incorrect");
+    }
 }
