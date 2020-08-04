8f2ae98fb41e59b7c81d73bd4490b0b235d3d3af
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-08-04 19:03:09 +0100

    first commit on Forms exercise

---
+++ src/main/java/pages/WysiwygEditorPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class WysiwygEditorPage {
+
+    private WebDriver driver;
+
+    public WysiwygEditorPage(WebDriver driver){
+        this.driver = driver;
+    }
+}
