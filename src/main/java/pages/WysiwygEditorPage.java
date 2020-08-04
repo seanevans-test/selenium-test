a50be674c23f165cf5052a440509edb7049199ff
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-08-04 19:09:13 +0100

    added locater method to find the form on the page ready to input data

--- src/main/java/pages/WysiwygEditorPage.java
+++ src/main/java/pages/WysiwygEditorPage.java
@@ -5,8 +5,13 @@ import org.openqa.selenium.WebDriver;
 public class WysiwygEditorPage {
 
     private WebDriver driver;
+    private String editorIframeId = "mce_0_ifr";
 
     public WysiwygEditorPage(WebDriver driver){
         this.driver = driver;
+    }
+
+    private void switchToEditArea(){
+        driver.switchTo().frame(editorIframeId);
     }
 }
