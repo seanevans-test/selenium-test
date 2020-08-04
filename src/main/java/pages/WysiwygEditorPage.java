5185f72e4b9238b5226e4b8302c796c22ed7e4b4
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-08-04 19:26:51 +0100

    finished adding the framework page elements ready fro the test

--- src/main/java/pages/WysiwygEditorPage.java
+++ src/main/java/pages/WysiwygEditorPage.java
@@ -8,6 +8,7 @@ public class WysiwygEditorPage {
     private WebDriver driver;
     private String editorIframeId = "mce_0_ifr";
     private By textArea = By.id("tinymce");
+    private By decreaseIndentButton = By.cssSelector("#mceu_12 button");
 
     public WysiwygEditorPage(WebDriver driver){
         this.driver = driver;
@@ -16,6 +17,24 @@ public class WysiwygEditorPage {
     public void clearTextArea(){
         switchToEditArea();
         driver.findElement(textArea).clear();
+        switchToMainArea();
+    }
+
+    public void setTextArea(String text){
+        switchToEditArea();
+        driver.findElement(textArea).sendKeys(text);
+        switchToMainArea();
+    }
+
+    public String getTextFromEditor(){
+        switchToEditArea();
+        String text = driver.findElement(textArea).getText();
+        switchToMainArea();
+        return text;
+    }
+
+    public void decreaseIndention(){
+        driver.findElement(decreaseIndentButton).click();
     }
 
     private void switchToEditArea(){
