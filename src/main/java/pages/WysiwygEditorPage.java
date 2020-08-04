5e8fe1115c59fe6004fb8ab37b217ea8023f1232
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-08-04 19:18:51 +0100

    added methods which select an iFrame, clears it and jumps back out of it.

--- src/main/java/pages/WysiwygEditorPage.java
+++ src/main/java/pages/WysiwygEditorPage.java
@@ -1,17 +1,28 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class WysiwygEditorPage {
 
     private WebDriver driver;
     private String editorIframeId = "mce_0_ifr";
+    private By textArea = By.id("tinymce");
 
     public WysiwygEditorPage(WebDriver driver){
         this.driver = driver;
     }
 
+    public void clearTextArea(){
+        switchToEditArea();
+        driver.findElement(textArea).clear();
+    }
+
     private void switchToEditArea(){
         driver.switchTo().frame(editorIframeId);
+    }
+
+    private void switchToMainArea(){
+        driver.switchTo().parentFrame();
     }
 }
