1b91126a2850d32c177d83590ef59d45e4604016
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-08-10 19:18:53 +0100

    added method to click on the start button

--- src/main/java/pages/DynamicLoadingExample1Page.java
+++ src/main/java/pages/DynamicLoadingExample1Page.java
@@ -6,8 +6,13 @@ import org.openqa.selenium.WebDriver;
 public class DynamicLoadingExample1Page {
 
     private WebDriver driver;
+    private By startButton = By.cssSelector("#start button");
 
     public DynamicLoadingExample1Page(WebDriver driver){
         this.driver = driver;
+    }
+
+    public void clickStart(){
+        driver.findElement(startButton).click();
     }
 }
