1e9d675300d99f492c68949dcb2f91ea9491a3dc
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-08-10 19:54:02 +0100

    added an alternative way of adding in a wait through FluentWait

--- src/main/java/pages/DynamicLoadingExample1Page.java
+++ src/main/java/pages/DynamicLoadingExample1Page.java
@@ -1,9 +1,13 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class DynamicLoadingExample1Page {
 
@@ -22,6 +26,13 @@ public class DynamicLoadingExample1Page {
         wait.until(ExpectedConditions.invisibilityOf(
                 driver.findElement(loadingIndicator)));
     }
+
+  //  FluentWait wait = new FluentWait(driver)
+  //          .withTimeout(Duration.ofSeconds(5))
+  //          .pollingEvery(Duration.ofSeconds(1))
+  //          .ignoring(NoSuchElementException.class);
+  //  wait.until(ExpectedConditions.invisibilityOf(
+  //          driver.findElement(loadingIndicator)));
 
     public String getLoadedText(){
         return driver.findElement(loadedText).getText();
