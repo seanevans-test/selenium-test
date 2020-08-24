dfe15e9dd1a8c244858f88d32843473e810b8e5c
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-08-24 19:43:04 +0100

    set up the Webdriver connection and the selenium webelements or the username/password

--- src/test/java/CucumberTests/CucumberTests.java
+++ src/test/java/CucumberTests/CucumberTests.java
@@ -3,16 +3,30 @@ package CucumberTests;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.Driver;
 
 public class CucumberTests {
 
-    @Given("I am in the login page of the sports application")
-    public void i_am_in_the_login_page_of_the_sports_application(){
+    private WebDriver driver;
 
+    @Given("I am in the login page of the Para Bank application")
+    public void i_am_in_the_login_page_of_the_Para_Bank_application(){
+
+        System.setProperty("webdriver.chrome.driver", "C://Drivers//chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://parabanks.parasoft.com/parabank.index.htm");
     }
 
     @When("I enter valid credentials")
     public void i_enter_valid_credentials(){
+
+        driver.findElement(By.name("username")).sendKeys("tautester");
+        driver.findElement(By.name("password")).sendKeys("password");
+        driver.findElement(By.name("username")).submit();
 
     }
 
