5e43cd63f2c4b5d77969fbc6f66b5740f013a5ff
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-08-24 19:33:56 +0100

    first commit on cucumber tests, set out the Given, When and Then statements of the test

---
+++ src/test/java/CucumberTests/CucumberTests.java
@@ -0,0 +1,23 @@
+package CucumberTests;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CucumberTests {
+
+    @Given("I am in the login page of the sports application")
+    public void i_am_in_the_login_page_of_the_sports_application(){
+
+    }
+
+    @When("I enter valid credentials")
+    public void i_enter_valid_credentials(){
+
+    }
+
+    @Then("I should be taken to the Posts page")
+    public void i_should_be_taken_to_the_Posts_page(){
+
+    }
+}
