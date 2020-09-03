12f5b1f325fbdc2f351d43fd00e32174141cb330
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-09-03 18:54:34 +0100

    added 2 tests, 1 for the LargeAndDeepDom, 1 for the Infinite Scroll

--- src/test/java/JavaScript/JavaScriptTests.java
+++ src/test/java/JavaScript/JavaScriptTests.java
@@ -1,8 +1,18 @@
 package JavaScript;
 
 import base.BaseTests;
+import org.testng.annotations.Test;
 
 public class JavaScriptTests extends BaseTests {
 
+    @Test
+    public void testScrollToTable(){
+        homePage.clickLargeAndDeepDom().scrollToTable();
 
+    }
+
+    @Test
+    public void testScrollToFifthParagraph(){
+        homePage.clickInfiniteScrollPage().scrollToParagraph(5);
+    }
 }
