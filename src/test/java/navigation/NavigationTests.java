2008ede736dfedf4d7170895df1970ab2b36648d
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-09-03 19:14:56 +0100

    added to the testNG test to go back, go forward, refresh page and to go to a specified url

--- src/test/java/navigation/NavigationTests.java
+++ src/test/java/navigation/NavigationTests.java
@@ -1,8 +1,16 @@
 package navigation;
 
 import base.BaseTests;
+import org.testng.annotations.Test;
 
 public class NavigationTests extends BaseTests {
 
-
+    @Test
+    public void testNavigator(){
+        homePage.clickDynamicLoading().clickExample1();
+        getWindowManager().goBack();
+        getWindowManager().goForward();
+        getWindowManager().refreshPage();
+        getWindowManager().goTo("https://google.co.uk");
+    }
 }
