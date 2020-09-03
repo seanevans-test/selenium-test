33f2dd13e73586d722805c5f4ba91333194526e9
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-09-03 18:36:58 +0100

    first commit on InfiniteScrollPage

--- src/main/java/pages/InfiniteScrollPage.java
+++ src/main/java/pages/InfiniteScrollPage.java
@@ -1,4 +1,12 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class InfiniteScrollPage {
+
+    private WebDriver driver;
+    By textBlocks = By.className("iscroll-added");
+
+    public InfiniteScrollPage(WebDriver driver){this.driver = driver;}
 }
