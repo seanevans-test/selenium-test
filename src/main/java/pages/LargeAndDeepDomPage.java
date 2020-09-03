3df210be347dbf748c59b0d3c6dab598ad5fcb72
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-09-03 18:20:08 +0100

    added method to scroll to the table on the page

--- src/main/java/pages/LargeAndDeepDomPage.java
+++ src/main/java/pages/LargeAndDeepDomPage.java
@@ -1,7 +1,9 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LargeAndDeepDomPage {
 
@@ -9,4 +11,10 @@ public class LargeAndDeepDomPage {
     private By table = By.id("large-table");
 
     public LargeAndDeepDomPage(WebDriver driver){this.driver = driver;}
+
+    public void scrollToTable(){
+        WebElement tableElement = driver.findElement(table);
+        String script = "arguments[0].scrollIntoView();";
+        ((JavascriptExecutor)driver).executeScript(script, tableElement);
+    }
 }
