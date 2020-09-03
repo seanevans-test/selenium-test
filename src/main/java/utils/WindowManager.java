1ba1f4e1c1d69699ad64bf87bb392e76b791f0c4
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-09-03 19:02:47 +0100

    first commit on App Navigation exercise

---
+++ src/main/java/utils/WindowManager.java
@@ -0,0 +1,14 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+
+public class WindowManager {
+
+    private WebDriver driver;
+    private WebDriver.Navigation navigate;
+
+    public WindowManager(WebDriver driver){
+        this.driver = driver;
+        navigate = driver.navigate();
+    }
+}
