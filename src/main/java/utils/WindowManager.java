4bb498e1896fb1e5dd9f7606c679688783846ec6
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-09-03 19:05:54 +0100

    added methods to allow for app navigation

--- src/main/java/utils/WindowManager.java
+++ src/main/java/utils/WindowManager.java
@@ -11,4 +11,20 @@ public class WindowManager {
         this.driver = driver;
         navigate = driver.navigate();
     }
+
+    public void goBack(){
+        navigate.back();
+    }
+
+    public void goForward(){
+        navigate.forward();
+    }
+
+    public void refreshPage(){
+        navigate.refresh();
+    }
+
+    public void goTo(String url){
+        navigate.to(url);
+    }
 }
