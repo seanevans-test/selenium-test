dbca825d986c6ac5f36e71b38e877f044be7d80d
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-09-08 18:52:23 +0100

    added TestNG method on the basetest to take a screenshot at the end of the test

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,15 +1,15 @@
 package base;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 import utils.WindowManager;
 
+import java.io.File;
 import java.util.List;
 
 public class BaseTests {
@@ -34,6 +34,13 @@ public class BaseTests {
     @AfterClass
     public void tearDown() {
         driver.quit();
+    }
+
+    @AfterMethod
+    public void takeScreenshot(){
+        var camera = (TakesScreenshot)driver;
+        File screenshot = camera.getScreenshotAs(OutputType.FILE);
+        System.out.println("Screenshot taken: " + screenshot.getAbsolutePath());
     }
 
     public WindowManager getWindowManager(){
